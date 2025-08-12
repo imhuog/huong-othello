13aaa0917800ab5ded241f267d718e4dcd26f77e
huong <59523300+imhuog@users.noreply.github.com>
2025-08-12 10:22:12 +0700

    Update next.Config.js

--- frontend/next.Config.js
+++ frontend/next.Config.js
@@ -5,4 +5,16 @@ const nextConfig = {
   // Nếu bạn có các cấu hình khác, hãy giữ lại chúng ngoại trừ swcMinify
 };
 
+// frontend/next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+});
+
+module.exports = withPWA({
+  reactStrictMode: true
+});
+
 module.exports = nextConfig;
+
